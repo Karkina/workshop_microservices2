38c95a96f2b49347741aa513bafb351fa96cbc9d
Anoujean <anoujean.balachandran@kdetude.com>
2021-10-29 09:38:15 +0200

    update

--- snippets/models.py
+++ snippets/models.py
@@ -16,7 +16,7 @@ class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     nom = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
-    montant = models.DecimalField(decimal_places=2, max_digits=1000000)
+    montant = models.DecimalField(decimal_places=2, max_digits=900)
     devise = models.CharField(choices=DEVISE,default="Eur",max_length=20)
     zone = models.CharField(default="0",choices=STYLE_CHOICES, max_length=100)
     brower = models.CharField(default="0",max_length=100)
